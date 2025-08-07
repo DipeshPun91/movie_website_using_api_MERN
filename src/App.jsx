f4054a96db656ccc4e708997619db5ea9a2b5422
DipeshPun91 <pundipesh091@gmail.com>
2025-08-07 14:20:00 +0545

    Eleventh commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Header from "./component/partials/Header.jsx";
 import Movies from "./pages/Movies.jsx";
+import Footer from "./component/partials/Footer.jsx";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/movies" element={<Movies />} />
       </Routes>
+      <Footer />
     </div>
   );
 };
